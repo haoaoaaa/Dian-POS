23bdf1c72fe1e3ead3934139ae8263c95c9e131e
haoaoaaa <109651935@qq.com>
2026-09-24 01:07:57 +0800

    Level1.2八分之七成品，扫码有bug

--- pos.c
+++ pos.c
@@ -17,7 +17,7 @@ int main() {
     printf("---七一一便利店POS系统---\n");
     Item items[3]={
     {"Cola","001",3.50,50},
-    {"Lollipop","002",o.50,100},
+    {"Lollipop","002",0.50,100},
     {"Noodles","003",6.00,20},
     };
     int item_count=3;
@@ -31,16 +31,16 @@ int main() {
             else if (strcmp(input,"prices")==0){
                 printf("Item     Code    Price    Stock\n");
                 for (int i =0;i<item_count;i++){
-                    printf("%-9s %s %.2f %d\n",item[i].name,item[i].code,item[i].price,item[i].stock);
+                    printf("%-9s %s %.2f %d\n",items[i].name,items[i].code,items[i].price,items[i].stock);
                 }
             }
             else if (strcmp(input,"print")==0){
-                printf("Receipt"\n);
+                printf("Receipt\n");
                 printf("Item    Price    Quantity    Littletotal\n");
                 printf("----------------------------------------\n");
                 float total=0;
                 for (int j=0;j<cart_count;j++){
-                    float littltotal=cart[j].item.price*cart[j].quantity;
+                    float littletotal=cart[j].item.price*cart[j].quantity;
                     total=littletotal+total;
                     printf("%-9s %.2f x %d = %.2f\n",cart[j].item.name,cart[j].item.price,cart[j].quantity,littletotal);
                 }
@@ -48,28 +48,28 @@ int main() {
                 printf("Total:%.2f\n",total);
             }
             else if (strcmp(input,"checkout")==0){
-                printf("Receipt"\n);
+                printf("Receipt\n");
                 printf("Item    Price    Quantity    Littletotal\n");
                 printf("----------------------------------------\n");
                 float total=0;
                 for (int j=0;j<cart_count;j++){
-                    float littltotal=cart[j].item.price*cart[j].quantity;
+                    float littletotal=cart[j].item.price*cart[j].quantity;
                     total=littletotal+total;
                     printf("%-9s %.2f x %d = %.2f\n",cart[j].item.name,cart[j].item.price,cart[j].quantity,littletotal);
                 }
-                car_count=0;
+                cart_count=0;
                 printf("----------------------------------------\n");
                 printf("Total:%.2f\n",total);
-                printf("Thank you.\n")
+                printf("Thank you.\n");
             }
-            else if(strcmp(input,"drop"){
+            else if(strcmp(input,"drop")==0){
                 cart_count=0;
                 printf("已清空\n");
             }
             else{
                 int is_minus=0;
                 char code[50];
-                if (strcmp(input[0],"-")==0){
+                if (input[0]=='-'){
                     is_minus=1;
                     strcpy(code,input+1);
                 }
@@ -81,13 +81,33 @@ int main() {
                     if(strcmp(code,items[k].code)==0){
                     found=1;
                     }
+                    for(int x=0;x<cart_count;x++){
+                        int cart_idx=-1;
+                        if(x==cart_idx){
+                            break;
+                    }
+                if(is_minus==1){
+                   if(cart_idx!=-1){
+                       cart[cart_idx].quantity--;
+                       if (cart[cart_idx].quantity<=0){
+                           cart[cart_idx]=cart[cart_count -1];
+                           cart_count--;
+                       }
+                   }
                 }
-                int cart_idx=-1;
-                for(int x=0,x<cart_count;x++){
-                    if(j==cart_idx){
-                        break;
+                else{
+                    if(cart_idx!=-1){
+                        cart[cart_idx].quantity++;
+                    }
+                    else{
+                        if(cart_count<50){
+                            cart[cart_count].item=items[k];
+                            cart[cart_count].quantity=1;
+                            cart_count++;}}
+                        }
                     }
                 }
-                if(is_minus==1){
-                   if(cart_idx!=-1)
-                       cart_count
+            }
+       }
+       return 0;
+}
