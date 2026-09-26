71b9cfe405479e73e8ea187c4a03cf721705d565
haoaoaaa <109651935@qq.com>
2026-09-26 14:12:36 +0800

    Level2.1大功告成

--- pos.c
+++ pos.c
@@ -24,116 +24,182 @@ int main() {
     };
     int item_count=3;
     int order_id = 1;
+    int is_admin=0;
     while(1){
-        printf(">");
-        scanf("%s",input);
-            if (strcmp(input,"exit")==0||strcmp(input,"quit")==0){
+        if(is_admin==1){
+            printf("admin>");
+            fgets(input,50,stdin);
+            input[strcspn(input,"\n")]=0;
+            char cmd[20],code[10];
+            float new_price;
+            int new_stock;
+            sscanf(input,"%s %s %f",cmd,code,&new_price);
+            if(strcmp(cmd,"setprice")==0){
+                for(int i=0;i<item_count;i++){
+                    if(strcmp(code,items[i].code)==0){
+                        items[i].price=new_price;
+                        printf("Price updated.\n");
+                    }
+                }
+            }
+            else if(strcmp(cmd,"back")==0){
+                is_admin=0;
                 printf("Bye.\n");
-                break;
             }
-            else if (strcmp(input,"prices")==0){
-                printf("Item     Code    Price    Stock\n");
-                for (int i =0;i<item_count;i++){
-                    printf("%-9s %s %.2f %d\n",items[i].name,items[i].code,items[i].price,items[i].stock);
-                }
-            }
-            else if (strcmp(input,"print")==0){
-                printf("Receipt\n");
-                printf("Item    Price    Quantity    Littletotal\n");
-                printf("----------------------------------------\n");
-                float total=0;
-                for (int j=0;j<cart_count;j++){
-                    float littletotal=cart[j].item.price*cart[j].quantity;
-                    total=littletotal+total;
-                    printf("%-9s   %.2f   x   %d   =   %.2f\n",cart[j].item.name,cart[j].item.price,cart[j].quantity,littletotal);
-                }
-                printf("----------------------------------------\n");
-                printf("Total:%.2f\n",total);
-            }
-            else if (strcmp(input,"checkout")==0){
-                time_t t = time(NULL);
-                struct tm *tm_info = localtime(&t);
-                char time_str[64];
-                strftime(time_str,sizeof(time_str),"%Y-%m-%d %H:%M:%S",tm_info);
-                printf("Receipt\n");
-                printf("Item    Price    Quantity    Littletotal\n");
-                printf("----------------------------------------\n");
-                float total=0;
-                FILE *fp = fopen("sales.txt","a");
-                    if(fp == NULL){
-                        printf("文件打开失败\n");
+            else if(strcmp(cmd,"itemadd")==0){
+                char name[50],code[10];
+                float new_prices;
+                int result = sscanf(input,"%s %s %s %f",cmd,code,name,&new_price);
+                    if(result==4){
+                        printf("%s(%s) added.\n",name,code);
+                        strcpy(items[item_count].code,code);
+                        strcpy(items[item_count].name,name);
+                        items[item_count].price=new_price;
+                        items[item_count].stock=50;
+                        item_count++;
                     }
-                    else{
-                        fprintf(fp,"%d, %s, ",order_id,time_str);
-                        for (int j=0;j<cart_count;j++){
-                            float littletotal=cart[j].item.price*cart[j].quantity;
-                            total=littletotal+total;
-                            printf("%-9s   %.2f   x   %d   =   %.2f\n",cart[j].item.name,cart[j].item.price,cart[j].quantity,littletotal);
-                            fprintf(fp,"%s x%d, ",cart[j].item.name,cart[j].quantity);
-                        }
+                    else {
+                        printf("Error:Invalid command format!\n");
                     }
-                    cart_count=0;
-                    printf("----------------------------------------\n");
-                    printf("Total:%.2f\n",total);
-                    printf("Thank you.\n");
-                    fprintf(fp,"Total: %.2f\n",total);
-                    fclose(fp);
-                    order_id++;
-                    printf("Sales record saved.\n");
             }
-            else if(strcmp(input,"drop")==0){
-                cart_count=0;
-                printf("已清空\n");
+            else if(strcmp(cmd,"itemdel")==0){
+                for(int i =0;i<item_count;i++){
+                    if(strcmp(code,items[i].code)==0){
+                        items[i]=items[item_count-1];
+                        item_count--;
+                        printf("Item removed.\n");
+                        break;
+                    }
+                }
             }
             else{
-                int is_minus=0;
-                char code[50];
-                if (input[0]=='-'){
-                    is_minus=1;
-                    strcpy(code,input+1);
+                printf("Unkown admin command.\n");
+            }
+        }
+        else{
+            printf(">");
+            scanf("%s",input);
+                if (strcmp(input,"exit")==0||strcmp(input,"quit")==0){
+                    printf("Bye.\n");
+                    break;
                 }
-                else{
-                    strcpy(code,input);
+                else if(strcmp(input,"admin")==0){
+                    char password[20];
+                    printf("Password:");
+                    scanf("%s",password);
+                    if (strcmp(password,"Aa20080625")==0){
+                        is_admin=1;
+                        getchar();
+                        printf("Admin mode.\n");
+                    }
+                    else{
+                        printf("Wrong password.\n");
+                    }
                 }
-                int found=0;
-                for(int k =0;k<item_count;k++){
-                    if(strcmp(code,items[k].code)==0){
-                    found=1;
-                    int cart_idx=-1;
-                        for(int x=0;x<cart_count;x++){
-                            if(strcmp(code,cart[x].item.code)==0){
-                                cart_idx=x;
-                                break;
-                            }
-                        }
-                        if(is_minus==1){
-                            if(cart_idx!=-1){
-                               cart[cart_idx].quantity--;
-                                if (cart[cart_idx].quantity<=0){
-                                    cart[cart_idx]=cart[cart_count -1];
-                                    cart_count--;
-                                }
-                            }
+                else if (strcmp(input,"prices")==0){
+                    printf("Item     Code    Price    Stock\n");
+                    for (int i =0;i<item_count;i++){
+                        printf("%-9s %s %.2f %d\n",items[i].name,items[i].code,items[i].price,items[i].stock);
+                    }
+                }
+                else if (strcmp(input,"print")==0){
+                    printf("Receipt\n");
+                    printf("Item    Price    Quantity    Littletotal\n");
+                    printf("----------------------------------------\n");
+                    float total=0;
+                    for (int j=0;j<cart_count;j++){
+                        float littletotal=cart[j].item.price*cart[j].quantity;
+                        total=littletotal+total;
+                        printf("%-9s   %.2f   x   %d   =   %.2f\n",cart[j].item.name,cart[j].item.price,cart[j].quantity,littletotal);
+                    }
+                    printf("----------------------------------------\n");
+                    printf("Total:%.2f\n",total);
+                }
+                else if (strcmp(input,"checkout")==0){
+                    time_t t = time(NULL);
+                    struct tm *tm_info = localtime(&t);
+                    char time_str[64];
+                    strftime(time_str,sizeof(time_str),"%Y-%m-%d %H:%M:%S",tm_info);
+                    printf("Receipt\n");
+                    printf("Item    Price    Quantity    Littletotal\n");
+                    printf("----------------------------------------\n");
+                    float total=0;
+                    FILE *fp = fopen("sales.txt","a");
+                        if(fp == NULL){
+                            printf("文件打开失败\n");
                         }
                         else{
-                            if(cart_idx!=-1){
-                                cart[cart_idx].quantity++;
-                            }
-                            else{
-                                if(cart_count<50){
-                                    cart[cart_count].item=items[k];
-                                    cart[cart_count].quantity=1;
-                                    cart_count++;
-                                }
-                                else{
-                                    printf("购物车已满\n");
-                                }
+                            fprintf(fp,"%d, %s, ",order_id,time_str);
+                            for (int j=0;j<cart_count;j++){
+                                float littletotal=cart[j].item.price*cart[j].quantity;
+                                total=littletotal+total;
+                                printf("%-9s   %.2f   x   %d   =   %.2f\n",cart[j].item.name,cart[j].item.price,cart[j].quantity,littletotal);
+                                fprintf(fp,"%s x%d, ",cart[j].item.name,cart[j].quantity);
                             }
                         }
-                        break;
-                   }
+                        cart_count=0;
+                        printf("----------------------------------------\n");
+                        printf("Total:%.2f\n",total);
+                        printf("Thank you.\n");
+                        fprintf(fp,"Total: %.2f\n",total);
+                        fclose(fp);
+                        order_id++;
+                        printf("Sales record saved.\n");
                 }
-           }
-       }
-       return 0;
+                else if(strcmp(input,"drop")==0){
+                    cart_count=0;
+                    printf("已清空\n");
+                }
+                else{
+                    int is_minus=0;
+                    char code[50];
+                    if (input[0]=='-'){
+                        is_minus=1;
+                        strcpy(code,input+1);
+                    }
+                    else{
+                        strcpy(code,input);
+                    }
+                    int found=0;
+                    for(int k =0;k<item_count;k++){
+                        if(strcmp(code,items[k].code)==0){
+                        found=1;
+                        int cart_idx=-1;
+                            for(int x=0;x<cart_count;x++){
+                                if(strcmp(code,cart[x].item.code)==0){
+                                    cart_idx=x;
+                                    break;
+                                }
+                            }
+                            if(is_minus==1){
+                                if(cart_idx!=-1){
+                                    cart[cart_idx].quantity--;
+                                    if (cart[cart_idx].quantity<=0){
+                                        cart[cart_idx]=cart[cart_count -1];
+                                        cart_count--;
+                                    }
+                                }
+                            }
+                            else{
+                                if(cart_idx!=-1){
+                                    cart[cart_idx].quantity++;
+                                }
+                                else{
+                                    if(cart_count<50){
+                                        cart[cart_count].item=items[k];
+                                        cart[cart_count].quantity=1;
+                                        cart_count++;
+                                    }
+                                    else{
+                                         printf("购物车已满\n");
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+          }
     }
+    return 0;
+}
